66b525f2c9ff7b42f749ff74ada01923bf843407
kempelen <lion@netngine.hu>
2022-04-10 22:33:11 +0200

    Apply system resource limits on incrond version
    Will use a lockfile to run only one process at once.
    Will use "nice" to use less system resources.
    Will put a memory limit on itself.

--- render-backend/bin/rendermymap-incron.sh
+++ render-backend/bin/rendermymap-incron.sh
@@ -6,5 +6,8 @@
 #/home/render/web/rendermymap/jobs IN_CLOSE_WRITE /home/render/bin/rendermymap-incron.sh $@/$#
 
 cd /home/render/bin/
+lockfile -r 75 -l 600 .rendering-lock || exit
 . rendermymap.config
-./rendermymap.sh $* >>$LOGFILE 2>&1
+ulimit -Sv1310720
+nice -n 20 ./rendermymap.sh $* >>$LOGFILE 2>&1
+rm -f .rendering-lock
